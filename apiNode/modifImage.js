e416b88044acdda234eba003ad8557554a4b4d1f
titou <titouanmagnin2@gmail.com>
2023-03-19 13:53:13 +0100

    aller-retour image ok avec telechargement possible

--- apiNode/modifImage.js
+++ apiNode/modifImage.js
@@ -1,32 +1,42 @@
 import fileUpload from "express-fileupload";
 import express from "express";
 import {Image} from "image-js";
+import fs from "fs";
 
 const router = express.Router();
 const __dirname = process.cwd()+"\\tmp\\"
 
+var number = 0;
+
 router.use(fileUpload({debug:true}))
 router.use(async (req, res) => {
     // console.log(`${process.cwd()}\\tmp`)
-        console.log(req.files)
-        console.log(req.body)
+    //     console.log(req.files)
+        const param = req.body
+        console.log(param)
         const file = req.files.file
-        console.log(file.data)
-        console.log(typeof file.data)
-        await file.mv(__dirname+file.name)
+        const i = file.name.lastIndexOf(".")
+        const newNom = file.name.substring(0,i)+"_"+number+file.name.substring(i)
+        // console.log(file.data)
+        // console.log(typeof file.data)
+        await file.mv(__dirname+newNom)
         console.log("fin")
 
-        const image = await Image.load(__dirname+file.name);
-        const newImage = image.grey();
-        newImage.save(__dirname+file.name);
+        var image = await Image.load(__dirname+newNom);
+        if (param.p_gris==="true")
+            image = image.grey();
+        console.log(__dirname+newNom)
+        await image.save(__dirname + newNom);
+        number++;
+            // setTimeout(()=>{
+            //         fs.unlink(__dirname+newNom,(err)=>{if (err) console.log(err)})
+            // },1000*30)
 
-        res.status(200).send({message:"ok",nomFichier:file.name,size:file.size});
-        // res.status(200).sendFile(__dirname+file.name,{
-        //     headers: {
-        //         'Content-Type': 'image/png',
-        //         'Content-Disposition': 'attachment; filename="image.png"'
-        //     }
-        // })
+        // res.status(200).send({message:"ok",nomFichier:newNom,size:file.size,width:image.width,height:image.height});
+        res.status(200).sendFile(__dirname+newNom,()=>{
+                fs.unlink(__dirname+newNom,(err)=>{if (err) console.log(err)})
+        })
+        // res.download(__dirname+newNom)
 })
 
 export default router;
--- front_end/src/views/ModifImage.vue
+++ front_end/src/views/ModifImage.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
         <h1>ModifImage</h1>
-        <input type="file" id="file" ref="inputFile" @change="newFile">
+            <h3>Parametres : </h3>
+        <div style="display: flex">
+           <div style="width: 50%; padding: 10px; border: black 1px solid">
+                <input type="file" id="file" ref="inputFile" @change="newFile">
+                <br>
+                <img :src="image" alt="image" v-if="image!=null" style="width: 100%">
+           </div>
+            <div style="width: 50% ; padding: 10px; border: black 1px solid">
+                <label for="gris">En noir et blanc : </label>
+                <input type="checkbox" id="gris" name="gris" v-model="gris">
+                <br>
+                <label for="taille">Redimmensioner (en %) (100=rien) : </label>
+                <input type="number" id="taille" name="taille" v-model="taille" min="20" max="300" style="border: black 1px solid">
+                <br>
+                <label for="enAuto">Enregistrer automatiquement image resultat : </label>
+                <input type="checkbox" id="enAuto" name="enAuto" v-model="enAuto">
+                <br>
+            </div>
+        </div>
         <br>
-        <img :src="image" alt="image" width="200" height="200" v-if="image!=null">
+        <v-btn color="primary" @click="onSubmit">Changer</v-btn>
+        <v-btn v-if="derniereImage!=null" color="primary" @click="enregistrer(derniereImage,derniereImageName)">Enregistrer dernière image</v-btn>
         <br>
 <!--        <img :src="`data:image/png;base64,${imageFin}`" alt="image" width="200" height="200" v-if="imageFin!=null">-->
 <!--        <img :src="'http://localhost:3000/tmp/'+imageFin.nomFichier" alt="image" width="200" height="200" v-if="imageFin!=null">-->
-        <img :src="imageFin" alt="image" width="200" height="200" v-if="imageFin!=null">
-        <br>
-        <v-btn color="primary" @click="onSubmit">Changer</v-btn>
+        <div>
+            <h3>Sorties :</h3>
+            <img v-for="(img,index) in imageFin" :key="index"
+                :src="img" alt="image" style="margin: 10px; max-width: 500px">
+        </div>
 
     </div>
 </template>
@@ -22,18 +43,24 @@ export default {
     data() {
         return {
             image: null,
-            imageFin: null
+            imageFin: [],
+            derniereImage:null,
+            derniereImageName:"",
+            gris:false,
+            taille:100,
+            enAuto:true
         }
     },
     methods: {
         newFile() {
             let file = this.$refs.inputFile.files[0];
+            if (file!==undefined)
+                this.image=URL.createObjectURL(file);
             // let reader = new FileReader();
             // reader.readAsDataURL(file);
             // reader.onload = () => {
             //     this.image = reader.result;
             // }
-            this.image=URL.createObjectURL(file);
         },
         _arrayBufferToBase64( buffer ) {
             var binary = '';
@@ -49,20 +76,44 @@ export default {
             let file = this.$refs.inputFile.files[0];
             let formData = new FormData();
             formData.append('file', file);
-            formData.append('test',"hrgnrjkghntfklf")
+            formData.append('p_gris',this.gris)
+            formData.append('p_taille', this.taille)
             console.log(file)
             myaxios.post('/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
+                responseType: 'blob'//tres tres important si on veut recevoir un blob en reponse
             }).then((response) => {
-                // this.imageFin =HTMLImageElement.("http://localhost:3000/tmp/"+response.data.nomFichier)
-                this.imageFin ="http://localhost:3000/tmp/"+response.data.nomFichier +`?v=${new Date().getTime()}`
-                // console.log(this.imageFin)
-                console.log(response);
+                // this.imageFin.push("http://localhost:3000/tmp/"+response.data.nomFichier +`?v=${new Date().getTime()}`)
+                console.log(response)
+                this.downloadFiles(response, "imageModifiee.png", "png")
             }).catch((error) => {
                 console.log(error);
+                alert("impossible d'envoyer les données au serveur")
             })
+        },
+        enregistrer(file,file_name){
+            if (window.navigator.msSaveOrOpenBlob)
+                window.navigator.msSaveOrOpenBlob(file, file_name);
+            else {
+                var a = document.createElement("a"),
+                    url = URL.createObjectURL(file);
+                a.href = url;
+                a.download = file_name;
+                document.body.appendChild(a);
+                a.click();
+                setTimeout(function () {
+                    document.body.removeChild(a);
+                    window.URL.revokeObjectURL(url);
+                }, 0);
+            }
+        },
+        downloadFiles(data, file_name) {
+            var file = data.data
+            this.imageFin.push(URL.createObjectURL(file))
+            this.derniereImage=file
+            this.derniereImageName=file_name
+            if(this.enAuto) {
+                this.enregistrer(file,file_name)
+            }
         }
     }
 }
